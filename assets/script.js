9362bf59d20d9f7a644e41fb628e66f165f715ee
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-02-04 20:33:19 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,29 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+    return true;
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+    return true;
+}
+
+// update progress & timestamp
+function updateProgres() {
+    return true;
+}
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgres);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
